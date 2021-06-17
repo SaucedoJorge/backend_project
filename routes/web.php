639bf9a187d91f4088d0d6f10538e8ed99552a33
Jorge Saucedo <saucedo.jorgeluis98@gmail.com>
2021-06-16 22:12:20 -0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -14,8 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', "DashboardController@index"())->name('Dashboard');
-
 Route::prefix('Dashboard')->group(function () {
     Route::get('users/{id}', function ($id) {
         
