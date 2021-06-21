ea67faa69bcdab5889c4e1fa9b411de7662ec792
Jorge Saucedo <saucedo.jorgeluis98@gmail.com>
2021-06-20 21:48:13 -0300

    Test Layout
    NO ESTOY ENTENDIENDO NADA

--- resources/views/home/home.blade.php
+++ resources/views/home/home.blade.php
@@ -1,8 +1,7 @@
 @extends('base')
 @section('title') Inicio @endsection
 @section('content')
-    <div
-        class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-        <h1 class="h2">Dashboard</h1>
-    </div>
+        <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+            <h1 class="h2">Dashboard</h1>
+        </div>
 @endsection
